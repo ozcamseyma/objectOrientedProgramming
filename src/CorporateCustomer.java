04657385aa0719d04e13d4b6fbd2469e0f58b101
ozcamseyma <ozcamseyma@gmail.com>
2022-11-20 18:05:54 +0300

    20

--- src/CorporateCustomer.java
+++ src/CorporateCustomer.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class CorporateCustomer {
+public class CorporateCustomer extends Customer {
+
+
+    private String CompanyName;
+    private String TaxNumber;
+
+    public String getCompanyName() {
+        return CompanyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        CompanyName = companyName;
+    }
+
+    public String getTaxNumber() {
+        return TaxNumber;
+    }
+
+    public void setTaxNumber(String taxNumber) {
+        TaxNumber = taxNumber;
+    }
+
+
+
+
 }
